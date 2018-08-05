8ab1cadeff7692ffb9a4954754a06bec367c1c83
Sam Vendittelli <sam.vendittelli@hotmail.com>
2018-08-05 18:04:42 +0100

    Stop ability to execute arbritary code
    Previously an attacker could execute arbritary code from the command
    line. This possibility has been removed by hardcoding or parsing query
    parameters.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const shell = require("shelljs");
 const moment = require("moment");
+const express = require("express");
 const tokens = require("./tokens");
 
-const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -14,19 +14,25 @@ app.use((request, response, next) => {
 });
 
 app.delete("/off", (request, response) => {
-  eventType = request.query.eventType || "pi_test";
   token = tokens.IFTTT;
-  delay = request.query.delay || 1;
-  script = ["pi-off.sh", eventType, token, delay].join(" ");
 
+  if (request.query.delay) {
+    delay = parseInt(request.query.delay) !== NaN ? request.query.delay : 1;
+  } else {
+    delay = 1;
+  }
+  delay = Math.max(delay, 1);
+
+  script = ["pi-off.sh", "pi_off", token, delay].join(" ");
   shell.exec(script, { async: true, silent: true });
+
   response.statusCode = 204;
   response.json({});
 });
 
 app.listen(port, err => {
   if (err) {
-    return log('Error', err);
+    return log("Error", err);
   }
 
   log(`Server is listening on ${port}`);
